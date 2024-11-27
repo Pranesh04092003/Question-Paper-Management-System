9f6cf334801106630d0b4bb49042bc6519ff0c56
Pranesh RV <139009385+Pranesh04092003@users.noreply.github.com>
2024-11-27 21:28:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('/qnpaperssssssss');
+        await mongoose.connect('mongodb+srv://test123:testing2003@cluster0.abeugnm.mongodb.net/qnpaperssona');
         console.log('MongoDB Connected...');
     } catch (err) {
         console.error(err.message);
